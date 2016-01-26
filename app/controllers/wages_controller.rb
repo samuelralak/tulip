5f2b50ba876032ae300f88a110078ea2fb2d34c5
samuelralak <samuelralak@hotmail.com>
2016-01-27 02:18:01 +0300

    listing both active and inactive workers and removed painters that have not been assigned duties for that particular week

--- app/controllers/wages_controller.rb
+++ app/controllers/wages_controller.rb
@@ -4,10 +4,11 @@ class WagesController < ApplicationController
   before_action :set_permanent_painters, only: [:permanent, :painter_permanent]
 
   def weekly
-  	@painters = Painter.where(employment_type_id: EmploymentType.find_by(code: 'TEMPORARY').id, is_active: true)
+  	@painters = Painter.where(employment_type_id: EmploymentType.find_by(code: 'TEMPORARY').id)
   	@track_painters = TrackPainter.where(
   		['painter_id IN (?) AND week_number = ?', @painters.pluck(:id), @start_date.strftime("%U").to_i]
   	)
+    @painters = @painters.where('id IN (?)', @track_painters.pluck(:painter_id))
   	@sites_attended = TrackPainterItem.joins(:site)
   		.includes(:track_painter)
   		.where(date_attended: @start_date.end_of_week)
