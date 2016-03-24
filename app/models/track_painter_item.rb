ef7eaab0cf0ed7e7a2df14455bdf25ef42b79bd4
Masud <masudmaalim@yahoo.com>
2016-03-24 21:12:09 +0300

    fixed reset week

--- app/models/track_painter_item.rb
+++ app/models/track_painter_item.rb
@@ -8,8 +8,13 @@ class TrackPainterItem < ActiveRecord::Base
 
 	private
 		def update_weekly_total
-			track_painter = TrackPainter.find(self.track_painter_id)
-			weekly_total = track_painter.track_painter_items.sum(:daily_allowance)
-			track_painter.update_attributes!(weekly_total: weekly_total)
+			track_painters = TrackPainter.where(id: self.track_painter_id)
+
+			unless track_painters.empty?
+				track_painter = TrackPainter.find(self.track_painter_id)
+				weekly_total = track_painter.track_painter_items.sum(:daily_allowance)+track_painter.track_painter_items.sum(:daily_wage)
+				track_painter.update_attributes!(weekly_total: weekly_total)
+			end
 		end
+
 end
