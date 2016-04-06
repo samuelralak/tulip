3a555d2dd2800f1c29fa3beb5f9ec220b5ca2b7f
samuelralak <samuelralak@hotmail.com>
2016-04-06 08:49:58 +0300

    temporarily removed load_and_authorize_resource for invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,5 @@
 class InvoicesController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
   before_action :set_invoice_number, only: [:show, :edit, :update, :destroy]
 
   # GET /invoice_numbers
