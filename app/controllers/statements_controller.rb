3e88375c18649ac06abb58c19025923f00c10f1e
Masud <masudmaalim@yahoo.com>
2016-04-09 06:16:16 +0300

    ordering_controllers

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -10,9 +10,9 @@ class StatementsController < ApplicationController
     @materials = @site.materials.all 
     @paints = @site.paints.all 
     track_painter_ids = @site.track_painter_items.pluck(:track_painter_id)
-    painter_ids = TrackPainter.where('id IN (?)', track_painter_ids).pluck(:painter_id).count
-    #@painters = Painter.where('id IN (?)', painter_ids)
-    @painters =@site.track_painter_items.pluck(:track_painter_id).inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+    painter_ids = TrackPainter.where('id IN (?)', track_painter_ids).pluck(:painter_id).uniq
+    @painters = Painter.where('id IN (?)', painter_ids)
+    #@painters =@site.track_painter_items.pluck(:track_painter_id).inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
     @invoices = @site.invoices
     @payments = @site.payments
   end
