64ba8af0dad449c1e213d1102133629c3c519654
Masud <masudmaalim@yahoo.com>
2016-06-23 18:28:26 +0300

    Fixing statements

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -13,7 +13,7 @@ class StatementsController < ApplicationController
     painter_ids = TrackPainter.where('id IN (?)', track_painter_ids).pluck(:painter_id).uniq
     @painters = Painter.where('id IN (?)', painter_ids)
     #@painters =@site.track_painter_items.pluck(:track_painter_id).inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
-    @invoices = @site.invoices.order('year asc, month')
+    @invoices = @site.invoices.order('invoice_date asc')
     @payments = @site.payments.order('payment_date asc')
   end
 end
