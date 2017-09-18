ed5a132b58ce224516a37922b9f9eb3b36c420e8
Samuel Ralak <samuelralak@hotmail.com>
2017-09-18 10:37:12 +0300

    making a redeploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,12 +24,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml config/application.yml}
